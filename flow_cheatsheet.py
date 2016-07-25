ce6bd1391a47f05f2539e89880e53180b0700e8b
Eliot <saltycrane@gmail.com>
2016-07-25 14:47:41 -0700

    update to show module members; other misc changes.
     - show members of a module as a sub list
     - separate "private" types from public types
     - show var, class, type, module, etc.
     - update to v0.29.0
     - refactoring to support above changes

--- flow_cheatsheet.py
+++ flow_cheatsheet.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
+"""
+Usage: python flow_cheatsheet.py
+
+"""
 import math
 import os.path
 import re
 import urllib2
+from collections import namedtuple
 
 
-COMMIT = 'v0.27.0'
+COMMIT = 'v0.29.0'
 GITHUB_DIR = 'https://github.com/facebook/flow/blob/{commit}/lib/'.format(commit=COMMIT)
 RAW_DIR = 'https://raw.githubusercontent.com/facebook/flow/{commit}/lib/'.format(commit=COMMIT)
 FILES = [
@@ -29,6 +34,8 @@ BUILTINS = [
 ]
 OUTPUT_FILE = 'dist/index.html'
 
+Result = namedtuple('Result', ['name', 'line_no', 'members', 'filename', 'type'])
+
 def main():
     results = [('builtins', 'Built-in types', BUILTINS)]
     for filename, heading in FILES:
@@ -36,8 +43,11 @@ def main():
         print url
         body = download_file(url)
         file_results = parse_file(body, filename)
-        file_results = sorted(file_results, key=lambda item: item[0].lower())
-        results.append((filename, heading, file_results))
+        file_results = post_process(file_results)
+        public_results = [result for result in file_results if not is_private(result)]
+        private_results = [result for result in file_results if is_private(result)]
+        results.append((filename, heading, public_results))
+        results.append((filename, heading + ' "private" types', private_results))
 
     write_output(results)
 
@@ -47,44 +57,87 @@ def download_file(url):
     return body
 
 def parse_file(body, filename):
+    """extract data from the file using regular expressions
+    """
     results = []
+    indentation = ''
+    module = None
     for line_no, line in enumerate(body.splitlines()):
-        match= re.search(r'^declare (class|interface|module) (?P<type>.+) {', line)
+        # start of a module
+        match= re.search(r'^declare (module) (?P<type>.+) {', line)
         if match:
-            results.append((match.group('type'), line_no))
+            indentation = r'\s+'
+            module = Result(
+                name=match.group('type'),
+                line_no=line_no,
+                members=[],
+                filename=filename,
+                type="module"
+            )
             continue
 
-        match= re.search(r'^declare var (?P<type>.+)', line)
-        if match:
-            results.append((match.group('type'), line_no))
+        # end of a module
+        match= re.search(r'^}', line)
+        if match and module:
+            indentation = ''
+            results.append(module)
+            module = None
             continue
 
-        match = re.search(r'type (?P<type>.+) =', line)
+        # choose whether to append matches to module.members or the top-level results list
+        if module:
+            appender = module.members
+        else:
+            appender = results
+
+        match = re.search('^' + indentation + r'declare (class) (?P<type>.+) {', line)
         if match:
-            results.append((match.group('type'), line_no))
+            appender.append(Result(match.group('type'), line_no, None, filename, "class"))
             continue
 
-    # filter out items starting with "_" and "$"
-    results = [
-        (item, line_no) for (item, line_no) in results
-        if not item.startswith('_') and not item.startswith('$')]
+        match = re.search('^' + indentation + r'declare (interface) (?P<type>.+) {', line)
+        if match:
+            appender.append(Result(match.group('type'), line_no, None, filename, "interface"))
+            continue
 
-    # remove unwanted stuff at end
-    def more_cleanup(item):
-        item = re.sub(r'extends.*$', '', item)
-        item = re.sub(r'=.*$', '', item)
-        item = re.sub(r': {$', '', item)
-        item = re.sub(r': \S+;$', '', item)
-        return item
+        match= re.search('^' + indentation + r'declare var (?P<type>.+)', line)
+        if match:
+            appender.append(Result(match.group('type'), line_no, None, filename, "var"))
+            continue
 
-    def create_github_link(line_no):
-        return '{github_link}#L{line_no}'.format(
-            github_link=(GITHUB_DIR + filename), line_no=(line_no + 1))
+        match = re.search('^' + indentation + r'type (?P<type>.+) =', line)
+        if match:
+            appender.append(Result(match.group('type'), line_no, None, filename, "type"))
+            continue
 
-    results = [
-        (more_cleanup(item), create_github_link(line_no))
-        for (item, line_no) in results]
+    return results
 
+def is_private(result):
+    # not sure if this is correct, but there was a reference to "$" here:
+    # http://sitr.us/2015/05/31/advanced-features-in-flow.html
+    return result.name.startswith('_') or '$' in result.name
+
+def post_process(results):
+    """sort and clean the results
+    """
+    def transform_result(result):
+        name, line_no, members, filename, type = result
+
+        if members:
+            members = post_process(members)
+
+        # remove unwanted stuff at end
+        name = re.sub(r'extends.*$', '', name)
+        name = re.sub(r'=.*$', '', name)
+        name = re.sub(r': .*$', '', name)
+
+        # remove quotes
+        name = name.strip('\'"')
+
+        return Result(name, line_no, members, filename, type)
+
+    results = [transform_result(result) for result in results]
+    results = sorted(results, key=lambda result: result.name.lower())
     return results
 
 def write_output(results):
@@ -98,6 +151,7 @@ def write_output(results):
     fout.write('The script to generate this list is on <a href="https://github.com/saltycrane/flow-cheatsheet">github</a>.\n')
     fout.write('Fixes welcome.\n')
     fout.write('</p>\n')
+    fout.write('<p>Note: I guess names with a <code>$</code> are supposed to be private so I separated them into their own section labeled X "private" types. See the note about that in <a href="http://sitr.us/2015/05/31/advanced-features-in-flow.html">http://sitr.us/2015/05/31/advanced-features-in-flow.html</a>.</p>\n')
     fout.write('<p>Flow version: {version}</p>\n'.format(version=COMMIT))
     fout.write('<ul class="list-unstyled">\n')
     fout.write('  <li><a href="#builtins">Built-in types</a></li>')
@@ -108,27 +162,82 @@ def write_output(results):
 
     for id_attr, heading, file_results in results:
 
-        grouped = group_in_columns(file_results)
+        if not file_results:
+            continue
+
+        lines = generate_output_lines(file_results)
+        grouped = group_in_columns(lines)
 
         fout.write('<div class="panel panel-default">\n')
         fout.write('<div class="panel-heading"><h4 id={id_attr}>{heading}</h4></div>\n'.format(
-            id_attr=id_attr, heading=heading, count=len(file_results)))
+            id_attr=id_attr, heading=heading, count=len(lines)))
         fout.write('<div class="panel-body">\n')
         fout.write('<div class="row">\n')
 
+        ul_tag_count = 0
         for group in grouped:
 
-            fout.write('<div class="col-sm-4"><ul class="list-unstyled">\n')
-            for item, link in group:
-                fout.write(
-                    '  <li><a href="{link}">{item}</a></li>\n'.format(
-                        link=link, item=html_escape(item)))
+            extra_ul_tags = '<ul>' * ul_tag_count
+            fout.write('<div class="col-sm-4"><ul>' + extra_ul_tags + '\n')
+            for line in group:
+                if '<ul>' in line:
+                    ul_tag_count += 1
+                if '</ul>' in line:
+                    ul_tag_count -= 1
+                fout.write(line + '\n')
 
-            fout.write('</div></ul>\n')
+            extra_ul_tags = '</ul>' * ul_tag_count
+            fout.write(extra_ul_tags + '</ul></div>\n')
 
-        fout.write('</div></div></div>')
+        fout.write('</div></div></div>\n')
 
     fout.close()
+
+def generate_output_lines(results):
+    """generate html output lines to write given a list of results
+    """
+    output= []
+    for result in results:
+        if len(result) == 2:
+            # result is a built-in result
+            name, url = result
+            link = generate_alink(name, url, None)
+            output.append('<li>{link}</li>'.format(link=link))
+        else:
+            lines = generate_result(result)
+            output.extend(lines)
+    return output
+
+def generate_result(result):
+    """recursively generate html lines to write starting at a single result
+    """
+    lines = []
+    name, line_no, members, filename, type = result
+    url = create_github_url(line_no, filename)
+    if members:
+        link = generate_alink(name, url, type)
+        lines.append('<li>{link}<ul>'.format(link=link))
+        for child_result in members:
+            child_lines = generate_result(child_result)
+            lines.extend(child_lines)
+        # mutate list so that <ul> tags don't count as items in the list when grouping columns
+        lines[-1] = '{last_line}</ul></li>'.format(last_line=lines[-1])
+    else:
+        link = generate_alink(name, url, type)
+        lines.append('<li>{link}</li>'.format(link=link))
+    return lines
+
+def generate_alink(name, url, type=None):
+    """return html for a single <a> link
+    """
+    alink = '<a href="{url}">{name}</a>'.format(url=url, name=html_escape(name))
+    if type:
+        alink += ' <small>({type})</small>'.format(type=type)
+    return alink
+
+def create_github_url(line_no, filename):
+    return '{github_url}#L{line_no}'.format(
+        github_url=(GITHUB_DIR + filename), line_no=(line_no + 1))
 
 def group_in_columns(items):
     N_COLUMNS = 3.0
