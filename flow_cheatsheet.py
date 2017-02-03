c3efa3e19ed5e40f3f9c4a11937bce1b64436c43
Eliot <saltycrane@gmail.com>
2017-02-03 07:38:36 -0800

    update for v0.37.4

--- flow_cheatsheet.py
+++ flow_cheatsheet.py
@@ -10,7 +10,7 @@ import urllib2
 from collections import namedtuple
 
 
-COMMIT = 'v0.35.0'
+COMMIT = 'v0.37.4'
 GITHUB_DIR = 'https://github.com/facebook/flow/tree/{commit}/'.format(commit=COMMIT)
 RAW_DIR = 'https://raw.githubusercontent.com/facebook/flow/{commit}/'.format(commit=COMMIT)
 FILES = [
